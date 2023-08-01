e2e12c2e04ea311c92bfc78f261f5573c5c69e36
jnm <jnm.engineers@gmail.com>
2023-08-01 17:05:22 +0300

    Merge remote-tracking branch 'fleetorigin/joseph' into joseph

--- src/main/java/com/jnm/cardealersms/controller/ProfileController.java
+++ src/main/java/com/jnm/cardealersms/controller/ProfileController.java
@@ -2,11 +2,13 @@ package com.jnm.cardealersms.controller;
 
 import com.jnm.cardealersms.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
 
+@Controller
 public class ProfileController {
 
     @Autowired private EmployeeService employeeService;
